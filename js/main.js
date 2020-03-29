228a204cb249bf35deb01b7c934e6e28b18973e6
Eugen <amateur.chess.2018@gmail.com>
2020-03-29 13:36:56 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
 jQuery(document).ready(function($){
-	function lang_process(t) {		
-		history.pushState(null, "", "/" + $(t).text() + "/");
+	function lang_process() {
+		var curr, lang_s = $("#langs_script");
+		if (!lang_s.is("script")) $("<script />", {src: "../js/langs/" + $(this).text() + ".js", id: "langs_script"}).appendTo("head");
+		else lang_s.attr("src", "../js/langs/" + $(this).text() + ".js");
+		history.pushState(null, "", "/" + $(this).text() + "/");
 		$("title").text(window.CurrentLang.title);
 		$('meta[name="description"]', 'meta[name="keywords"]').attr("content", window.CurrentLang.title);
 		$("h1").text(window.CurrentLang.first);
 		$("p").text(window.CurrentLang.second);
-		var curr = $("span", t.parentNode);
-		$("<a />", {href: "javascript:;", text: curr.text()}).click(lang_start).insertBefore(curr);
+		curr = $("span", this.parentNode);
+		$("<a />", {href: "javascript:;", text: curr.text()}).click(lang_process).insertBefore(curr);
 		curr.remove();
-		$("<span />", {text: $(t).text()}).insertBefore(t);
-		$(t).remove();
-	}	
-	function lang_start() {
-		var t = this, lang_s = $("#langs_script");
-		if (!lang_s.is("script")) $("<script />", {src: "../js/langs/" + $(this).text() + ".js", id: "langs_script"}).on("load", function(){
-			lang_process(t);
-		}).appendTo("head");
-		else lang_s.on("load", function(){ lang_process(t); }).attr("src", "../js/langs/" + $(this).text() + ".js");
+		$("<span />", {text: $(this).text()}).insertBefore(this);
+		$(this).remove();
 	}
-	$(".langs a").click(lang_start);
+	$(".langs a").click(lang_process);
 });
