880d1739cc0b61c95ae09d262f77b9bc10006048
Eugen <amateur.chess.2018@gmail.com>
2020-03-29 11:36:27 +0300

    langs

--- js/main.js
+++ js/main.js
@@ -3,5 +3,8 @@ jQuery(document).ready(function($){
 		document.getElementsByTagName("title")[0].innerHTML = window.CurrentLang.title;
 		document.getElementsByTagName("h1")[0].innerHTML = window.CurrentLang.first;
 		document.getElementsByTagName("p")[0].innerHTML = window.CurrentLang.second;
-	*/	
+	*/
+	$(".langs a").click(function() {
+		console.log($(this).text());
+	});
 });
