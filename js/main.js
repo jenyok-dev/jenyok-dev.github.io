6193f5d85138b87a7f6385bd474e2ac6f0b2a640
Eugen <amateur.chess.2018@gmail.com>
2020-03-29 13:10:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 jQuery(document).ready(function($){	
 	function lang_start() {
-		var lang_s = $("#langs_script");
-		if (!lang_s.is("script")) $("<script />", {src: "../js/langs/" + $(this).text() + ".js", id: "langs_script"}).load(lang_process).appendTo("head");
-		else lang_s.load(lang_process).attr("src", "../js/langs/" + $(this).text() + ".js");
+		var t = this, lang_s = $("#langs_script");
+		if (!lang_s.is("script")) $("<script />", {src: "../js/langs/" + $(this).text() + ".js", id: "langs_script"}).load(function(){ lang_process(t) }).appendTo("head");
+		else lang_s.load(function(){ lang_process(t) }).attr("src", "../js/langs/" + $(this).text() + ".js");
 	}
-	function lang_process() {
-		history.pushState(null, "", "/" + $(this).text() + "/");
+	function lang_process(t) {
+		history.pushState(null, "", "/" + $(t).text() + "/");
 		$("title").text(window.CurrentLang.title);
 		$('meta[name="description"]', 'meta[name="keywords"]').attr("content", window.CurrentLang.title);
 		$("h1").text(window.CurrentLang.first);
 		$("p").text(window.CurrentLang.second);
-		var curr = $("span", this.parentNode);
+		var curr = $("span", t.parentNode);
 		$("<a />", {href: "javascript:;", text: curr.text()}).click(lang_start).insertBefore(curr);
 		curr.remove();
-		$("<span />", {text: $(this).text()}).insertBefore(this);
-		$(this).remove();
+		$("<span />", {text: $(t).text()}).insertBefore(t);
+		$(t).remove();
 	}
 	$(".langs a").click(lang_start);
 });
