e9a968d165c144167829e2829b0c2fda0c96374a
Eugen <amateur.chess.2018@gmail.com>
2020-03-29 13:05:15 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 jQuery(document).ready(function($){	
-	function lang_process() {
+	function lang_start() {
 		var curr, lang_s = $("#langs_script");
-		if (!lang_s.is("script")) $("<script />", {src: "../js/langs/" + $(this).text() + ".js", id: "langs_script"}).appendTo("head");
-		else lang_s.attr("src", "../js/langs/" + $(this).text() + ".js");
+		if (!lang_s.is("script")) $("<script />", {src: "../js/langs/" + $(this).text() + ".js", id: "langs_script"}).load(lang_process).appendTo("head");
+		else lang_s.load(lang_process).attr("src", "../js/langs/" + $(this).text() + ".js");
+	}
+	function lang_process() {
 		history.pushState(null, "", "/" + $(this).text() + "/");
 		$("title").text(window.CurrentLang.title);
 		$('meta[name="description"]', 'meta[name="keywords"]').attr("content", window.CurrentLang.title);
 		$("h1").text(window.CurrentLang.first);
 		$("p").text(window.CurrentLang.second);
 		curr = $("span", this.parentNode);
-		$("<a />", {href: "javascript:;", text: curr.text()}).click(lang_process).insertBefore(curr);
+		$("<a />", {href: "javascript:;", text: curr.text()}).click(lang_start).insertBefore(curr);
 		curr.remove();
 		$("<span />", {text: $(this).text()}).insertBefore(this);
 		$(this).remove();
 	}
-	$(".langs a").click(lang_process);
+	$(".langs a").click(lang_start);
 });
