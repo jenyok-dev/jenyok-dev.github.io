70656a853794e70f6c4b63b37c96c1d90485cf93
Eugen <59449419+jenyok-dev@users.noreply.github.com>
2020-03-29 10:45:41 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,14 +3,15 @@ window.onload = function () {
 	var lang = localStorage.getItem("lang");
     if (lang == null) {
         lang = window.navigator.language || window.navigator.systemLanguage.substring(0, 2);
+	    if (lang != "uk" && lang != "ru" && lang != "en") lang = "en";
         localStorage.setItem("lang", lang);
 		console.log("from browser");
     } else console.log("from localStorage");
     console.log(lang);
     var scr = document.createElement("script");
     scr.setAttribute("src", "/js/langs/"+lang+".js");
-	scr.setAttribute("onload", "setLangs()");
-	document.getElementsByTagName("head")[0].appendChild(scr);
+    scr.setAttribute("onload", "setLangs()");
+    document.getElementsByTagName("head")[0].appendChild(scr);
 };
 
 function setLangs() {
