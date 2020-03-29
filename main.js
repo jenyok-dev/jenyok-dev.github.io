d506774d77f8f8bd2bda9a4d7cf492b0101f3324
Eugen <59449419+jenyok-dev@users.noreply.github.com>
2020-03-29 09:52:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.onload = function () {
     console.log("test js");
-    var lang = window.navigator.language || window.navigator.systemLanguage;
+    var lang = window.navigator.language || window.navigator.systemLanguage.substring(0, 2);
     console.log(lang);
 };
