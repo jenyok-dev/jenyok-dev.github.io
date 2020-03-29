7d0a4332286d8875fb742009f61f23d17c07db64
Eugen <amateur.chess.2018@gmail.com>
2020-03-29 13:08:21 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){	
 	function lang_start() {
-		var curr, lang_s = $("#langs_script");
+		var lang_s = $("#langs_script");
 		if (!lang_s.is("script")) $("<script />", {src: "../js/langs/" + $(this).text() + ".js", id: "langs_script"}).load(lang_process).appendTo("head");
 		else lang_s.load(lang_process).attr("src", "../js/langs/" + $(this).text() + ".js");
 	}
@@ -10,7 +10,7 @@ jQuery(document).ready(function($){
 		$('meta[name="description"]', 'meta[name="keywords"]').attr("content", window.CurrentLang.title);
 		$("h1").text(window.CurrentLang.first);
 		$("p").text(window.CurrentLang.second);
-		curr = $("span", this.parentNode);
+		var curr = $("span", this.parentNode);
 		$("<a />", {href: "javascript:;", text: curr.text()}).click(lang_start).insertBefore(curr);
 		curr.remove();
 		$("<span />", {text: $(this).text()}).insertBefore(this);
