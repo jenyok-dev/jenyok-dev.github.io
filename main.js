3df2913aaee566c9561bf275266280257b1e604a
Eugen <59449419+jenyok-dev@users.noreply.github.com>
2020-03-29 09:56:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 window.onload = function () {
-    console.log("test js");
-    var lang = window.navigator.language || window.navigator.systemLanguage.substring(0, 2);
-    console.log(lang);
+    if (!window.localStorage) return alert("your browser very old");
+	console.log("test js");
+	var lang = window.navigator.language || window.navigator.systemLanguage.substring(0, 2);
+	console.log(lang);
+    localStorage.setItem("TestStorageLang", lang);
+    console.log(localStorage.getItem("TestStorageLang"));
 };
