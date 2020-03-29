65cd15d302cac049186394d9d42df68b363af186
Eugen <59449419+jenyok-dev@users.noreply.github.com>
2020-03-29 10:04:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     if (!window.localStorage) return alert("your browser very old, please update or change");
-	var lang = localStorage.getItem("lang");
+    var lang = localStorage.getItem("lang");
     if (lang == null) {
 	lang = window.navigator.language || window.navigator.systemLanguage.substring(0, 2);
 	localStorage.setItem("lang", lang);
