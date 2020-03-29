6b447c1695b30ee4f46cf1866bcb201864ec7e15
Eugen <amateur.chess.2018@gmail.com>
2020-03-29 12:44:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function($){
-	/*
-		document.getElementsByTagName("title")[0].innerHTML = window.CurrentLang.title;
-		document.getElementsByTagName("h1")[0].innerHTML = window.CurrentLang.first;
-		document.getElementsByTagName("p")[0].innerHTML = window.CurrentLang.second;
-	*/
 	$(".langs a").click(function() {
-		console.log($(this).text());
+		if (!$("#lang_" + $(this).text()).is("script"))
+			$("<script />", {src: "../js/langs/" + $(this).text() + ".js", id: "lang_" + $(this).text()}).appendTo("head");
+		history.pushState(null, "", $(this).text()+"/");
+		$('meta[name="description"]', 'meta[name="keywords"]').attr("content", window.CurrentLang.title);
+		$("h1").text(window.CurrentLang.first);
+		$("p").text(window.CurrentLang.second);		
 	});
 });
