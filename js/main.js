077d899e588e483ba3d23b57721a76fb7c805fc0
Eugen <amateur.chess.2018@gmail.com>
2020-03-29 11:34:34 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,21 +1,7 @@
-window.onload = function () {
-    if (!window.localStorage) return alert("your browser very old, please update or change");
-	var lang = localStorage.getItem("lang");
-    if (lang == null) {
-        lang = window.navigator.language || window.navigator.systemLanguage.substring(0, 2);
-	    if (lang != "uk" && lang != "ru" && lang != "en") lang = "en";
-        localStorage.setItem("lang", lang);
-		console.log("from browser");
-    } else console.log("from localStorage");
-    console.log(lang);
-    var scr = document.createElement("script");
-    scr.setAttribute("src", "/js/langs/"+lang+".js");
-    scr.setAttribute("onload", "setLangs()");
-    document.getElementsByTagName("head")[0].appendChild(scr);
-};
-
-function setLangs() {
-    document.getElementsByTagName("title")[0].innerHTML = window.CurrentLang.title;
-    document.getElementsByTagName("h1")[0].innerHTML = window.CurrentLang.first;
-    document.getElementsByTagName("p")[0].innerHTML = window.CurrentLang.second;	
-}
+jQuery(document).ready(function($){
+	/*
+		document.getElementsByTagName("title")[0].innerHTML = window.CurrentLang.title;
+		document.getElementsByTagName("h1")[0].innerHTML = window.CurrentLang.first;
+		document.getElementsByTagName("p")[0].innerHTML = window.CurrentLang.second;
+	*/	
+});
